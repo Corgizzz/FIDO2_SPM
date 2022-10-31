5316c3c0ffa74ab580ceb929ec189eca079304aa
Corgi <boy860121@gmail.com>
2022-10-31 14:05:25 +0800

    1.0.1

--- Sources/FIDO2_SPM/FIDO2_SPM.swift
+++ Sources/FIDO2_SPM/FIDO2_SPM.swift
@@ -3,24 +3,9 @@ public struct FIDO2_SPM {
 
     public init() {
     }
-}
-public struct MyFirstSwiftPackageLibrary {
     
-    /// 這個變數的 Access Level
-    /// - getter -> public
-    /// - setter -> private
-    /// - getter 跟 setter 同時指定時，只需要注明 setter 為 (set)
-    public private(set) var text = "Hello, World!"
-
-    /// 初始化
-    public init() {
+    public func printDogName(name: String){
+        print("myDogName:\(name)")
     }
     
-    public static func sayHello() {
-        print("MyFirstSwiftPackageLibrary say Hello！")
-    }
-    
-    public static func inputAndOutput(input: String?) {
-        print("MyFirstSwiftPackageLibrary input \(String(describing: input)), and I output \(String(describing: input))")
-    }
 }
